9396a51704accbb4ebf19f952b688f99fe457668
root <w00tcolours2@gmail.com>
2020-04-12 15:39:52 +0300

    Updates

---
+++ precache-manifest.9ed03ed0e72db6a3bc98d55360a2c398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8216ed67c659b3780d3cfad602ee55c2",
+    "url": "/monsters-on-react/index.html"
+  },
+  {
+    "revision": "1837089007bda33b0a3d",
+    "url": "/monsters-on-react/static/css/main.29fb17cc.chunk.css"
+  },
+  {
+    "revision": "c2faac2bbfa16e11da7c",
+    "url": "/monsters-on-react/static/js/2.e142ffb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-on-react/static/js/2.e142ffb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1837089007bda33b0a3d",
+    "url": "/monsters-on-react/static/js/main.f3b7b1b3.chunk.js"
+  },
+  {
+    "revision": "55b7089be773dd853789",
+    "url": "/monsters-on-react/static/js/runtime-main.421b81a1.js"
+  }
+]);
